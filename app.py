ce2a41e0cc0282240242d9dd7934d5d1720cc4c6
Kyle Taylor <kyletaylored@gmail.com>
2020-03-09 13:32:11 -0500

    Add working site fetch

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from bs4 import BeautifulSoup as bs
+from datetime import datetime, timedelta
+import pytz
 import pandas as pd
 import requests
+import urllib.parse
 from pprint import pprint
+
 app = Flask(__name__)
-
-# Initialize sites
-api = API()
-
-@app.route('/')
-def hello():
-    return api.get_sites()
-
-@app.route('/api/sites')
-def api_sites():
-    return api.get_sites()
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 class API:
+    # TCEQ URLs
+    tceq_site_data_url = "https://www.tceq.texas.gov/cgi-bin/compliance/monops/daily_summary.pl"
     tceq_sites_url = "https://www17.tceq.texas.gov/tamis/index.cfm?fuseaction=report.site_list&sort=AQS_SITE_CD&order=asc&formSub=1&cams=checked&TCEQRegion=checked&siteName=checked&strAddr=checked&cityName=checked&zipCode=checked&cntyName=checked&lat=checked&long=checked&latLongType=dec&actDT=checked&urbanArea=checked&ownByName=checked&EPARegistered=checked&showActiveOnly=1&regFilter=&cntyFilter=&camsFilter="
-    
+
+    # Setting local TZ because all TCEQ monitors are in Texas.
+    local_tz = pytz.timezone('America/Chicago')
+
+    def __init__(self):
+        # Give default timestamp
+        self.timestamp = datetime.now().astimezone(tz=self.local_tz).timestamp()
+
+    def get_html_from_url(self, url, params=None, method=None):
+        # Use get or post
+        if method == 'post':
+            resp = requests.post(url, params=params)
+        else:
+            resp = requests.get(url, params=params)
+
+        # Debug URL
+        pprint(resp.url)
+
+        # Convert to HTML
+        html = bs(resp.text, 'html.parser')
+        return html.prettify()
 
     def get_sites(self):
         dfs = pd.read_html(self.tceq_sites_url)
@@ -27,15 +44,16 @@ class API:
         pprint(table)
         return table.to_json(orient="records")
 
-    
-    def get_html(self, timestamp=None):
+    def get_date(self, timestamp):
+        return datetime.fromtimestamp(timestamp).astimezone(tz=self.local_tz)
 
-        # Give default timestamp
+    def get_site_data(self, site_id=56, timestamp=None):
+        # Get timestamp
         if timestamp == None:
-            timestamp = datetime.now().astimezone(tz=self.local_tz).timestamp()
+            timestamp = self.timestamp
 
         # Generate date
-        date = datetime.fromtimestamp(timestamp).astimezone(tz=self.local_tz)
+        date = self.get_date(timestamp)
 
         # Prepare JSON
         params = {
@@ -43,11 +61,32 @@ class API:
             'user_month': date.month - 1,  # TCEQ has a weird offset.
             'user_day': date.day,
             'user_year': date.year,
-            'select_site': "|||" + str(self.site),
-            'time_format': "24hr"
+            'select_site': "|||" + str(site_id),
+            'time_format': '24hr'
         }
 
-        return requests.get(self.url, params=params).text
+        html = self.get_html_from_url(self.tceq_site_data_url, params=params)
+        dfs = pd.read_html(html, header=0)
+        table = dfs[-1]
+        # table.drop(table.index[-1], axis=1)
+        pprint(table)
+        return table.to_json()
+
+# Initialize sites
+api = API()
+
+@app.route('/api/sites')
+@cross_origin()
+def api_sites():
+    return api.get_sites()
+
+@app.route('/api/sites/<site_id>')
+@cross_origin()
+def api_site_data(site_id):
+    d = datetime.today() - timedelta(days=1)
+    ts = d.timestamp()
+    return api.get_site_data(site_id=site_id, timestamp=ts)
+
 
 
 if __name__ == '__main__':
